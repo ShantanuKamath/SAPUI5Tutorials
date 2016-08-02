92e29997ccf143d8927733efccfe0b9a12791095
shantanukamath <shantanu003@e.ntu.edu.sg>
2016-08-02 07:32:48 +0000

    Step 9 Component Configuration

---
+++ Walkthrough/webapp/Component.js
@@ -0,0 +1,30 @@
+sap.ui.define([
+	"sap/ui/core/UIComponent",
+	"sap/ui/model/ResourceModel",
+	"sap/ui/model/JSONModel"],
+	function(UIComponent, JSONModel, ResourceModel) {
+		"use strict";
+		return UIComponent.extend("sap.ui.demo.wt.Component", {
+			metadata : {
+				rootView : "sap.ui.demo.wt.view.App"
+			},
+			init : function(){
+				UIComponent.prototype.init.apply(this, arguments);
+				
+				var oData = {
+					recipient : {
+						name : "World"
+					}
+				};
+				
+				var oModel = new JSONModel(oData);
+				this.setModel(oModel);
+				
+				var i18nModel = new ResourceModel({
+					bundleName : "sap.ui.demo.wt.i18n.i18n"
+				});
+				
+				this.setModel(i18nModel, "i18n");
+			}
+		});
+	});
--- Walkthrough/webapp/controller/App.controller.js
+++ Walkthrough/webapp/controller/App.controller.js
@@ -1,24 +1,9 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/m/MessageToast",
-	"sap/ui/model/json/JSONModel",
-	"sap/ui/model/resource/ResourceModel"],
-	function(Controller, MessageToast, JSONModel, ResourceModel){
+	"sap/m/MessageToast"],
+	function(Controller, MessageToast){
 		"use strict";
 		return Controller.extend("sap.ui.demo.wt.controller.App", {
-			onInit : function() {
-			var oData = {
-				recipient : {
-					name : "World"
-				}
-			};
-			var oModel = new JSONModel(oData);
-			this.getView().setModel(oModel);
-			var i18nModel = new ResourceModel({
-				bundleName : "sap.ui.demo.wt.i18n.i18n"
-			});
-			this.getView().setModel(i18nModel, "i18n");
-			},
 			onShowHello : function (){
 				var oBundle =  this.getView().getModel("i18n").getResourceBundle();
 				var sRecipient = this.getView().getModel().getProperty("/recipient/name");
