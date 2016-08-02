9572c4b93b46f1bdf2144d68cc7cd25e7114b37b
shantanukamath <shantanu003@e.ntu.edu.sg>
2016-08-02 05:44:05 +0000

    Step 6

--- Walkthrough/webapp/controller/App.controller.js
+++ Walkthrough/webapp/controller/App.controller.js
@@ -1,9 +1,10 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"], function(Controller){
+	"sap/ui/core/mvc/Controller",
+	"sap/m/MessageToast"], function(Controller, MessageToast){
 		"use strict";
 		return Controller.extend("sap.ui.demo.wt.controller.App", {
 			onShowHello : function() {
-				alert("Hello World");
+				MessageToast.show("Hello World");
 			}			
 		});
 	});
